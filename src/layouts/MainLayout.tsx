e66b35361506f40626af5327cfd7d9f3e443d5f5
Alex Custódio <alex.scustod@gmail.com>
2023-05-09 18:11:04 +0000

    teste

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -73,11 +73,11 @@ export function MainLayout({ children, activeButton}: MainLayoutProps) {
               </p>
               <OKBRIcon className="lg:w-[11.33rem]"/>
             </div>
-            <Image
+            <img
               className="absolute min-[1792px]:max-w-none lg:-bottom-[18rem] md:-bottom-[200rem] lg:scale-75 xl:-bottom-[22rem] xl:scale-75 min-[1792px]:scale-100 min-[1792px]:-bottom-[28rem] left-2 min-[1792px]:-left-[6rem]"
               src="https://github.com/alex-custodio/exoneraa/blob/main/src/assets/images/girl-icon.png"
               alt={""}
-            ></Image>
+            ></img>
           </aside>
         </div>
         <main className="bg-[#F5F7FB]  w-full overflow-y-auto px-[2.875rem] pt-[3.25rem] pb-6 ">
