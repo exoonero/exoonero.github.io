fb35af4ca5bfa028e86523fc961b377a07dbd10e
Alex Custódio <alex.scustod@gmail.com>
2023-05-09 19:41:13 +0000

    adicionando icone

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -43,10 +43,9 @@ export function MainLayout({ children, activeButton}: MainLayoutProps) {
                 Confira as nomeações e exonerações de Alagoas
               </h1>
               <div className="flex gap-x-3 items-center">
-                <img
-                  src="/img-profiles.png"
-                  className="min-[1792px]:w-[7.56rem] lg:w-[6.301rem] min-[1792px]:h-[2.31rem] lg:h-[1.92rem]"
-                />
+                <Image
+                  src={"/exonera/img-profiles.png"}
+                  className="min-[1792px]:w-[7.56rem] lg:w-[6.301rem] min-[1792px]:h-[2.31rem] lg:h-[1.92rem]" alt={""}                />
                 <p className="text-white lg:text-base leading-4 font-normal ">
                   Juntos por uma sociedade mais transparente e participativa
                 </p>
@@ -72,10 +71,9 @@ export function MainLayout({ children, activeButton}: MainLayoutProps) {
               </p>
               <OKBRIcon className="lg:w-[11.33rem]"/>
             </div>
-            <img
+            <Image
               className="absolute min-[1792px]:max-w-none lg:-bottom-[18rem] md:-bottom-[200rem] lg:scale-75 xl:-bottom-[22rem] xl:scale-75 min-[1792px]:scale-100 min-[1792px]:-bottom-[28rem] left-2 min-[1792px]:-left-[6rem]"
-              src="/girl-icon.png"
-            />
+              src={"/girl-icon.png"} alt={""}            />
           </aside>
         </div>
         <main className="bg-[#F5F7FB]  w-full overflow-y-auto px-[2.875rem] pt-[3.25rem] pb-6 ">
