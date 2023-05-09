c7e9571d5459bffade43a60405d7a72a7c8d59b0
Alex Custódio <alex.scustod@gmail.com>
2023-05-09 14:01:18 +0000

    atualizando next.config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+const nextConfig = {
+    experimental: {
+        appDir: true
+    }
 
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
 }
 
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
-  },
-}
+module.exports = nextConfig
